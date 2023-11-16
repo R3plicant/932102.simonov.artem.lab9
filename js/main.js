be10dca507b381c6100da5ca4b0008b3323866d7
R3plicant <spawnhyperion@gmail.com>
2023-11-16 07:52:56 +0300

    lab9

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+const output = document.querySelector('#output');
+const del = document.querySelector('#del');
+const eq = document.querySelector('#eq');
+
+del.addEventListener('click', function(){
+    out = output.value;
+    out = out.slice(0, -1);
+    output.value = out;
+});
+
+eq.addEventListener('click', function(){
+    output.value = eval(output.value);
+});
 
 });
