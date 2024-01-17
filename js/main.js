723c6aac9b692e7f36574b1290ddc7f28a9d45ac
R3plicant <spawnhyperion@gmail.com>
2024-01-17 05:40:03 +0300

    Bug fixes

--- js/main.js
+++ js/main.js
@@ -9,7 +9,21 @@ del.addEventListener('click', function(){
     output.value = out;
 });
 
+function IsSpecial(c){
+	if ((c == ".") || (c == "*") || (c== "/") || (c == "+") || (c == "-"))
+		return 1;
+	else
+		return 0;
+}
+
 eq.addEventListener('click', function(){
+    var array = output.value;
+	for (i = 1; i < array.length; i++) {
+		if (IsSpecial(array[i]) && IsSpecial(array[i-1])){
+			alert("Error: Special characters cannot be placed twice in a row");
+			return 0;
+		}
+	}
     output.value = eval(output.value);
 });
 
